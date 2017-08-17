28373999c7eab6bf84f008e475518e33c03a2c12
udara.alwis <udara.alwis@tippstr.com>
2017-08-17 20:41:54 +0800

    initializing skiasharp

---
+++ XFSkiaSharpDemo/XFSkiaSharpDemo/XFSkiaSharpDemo/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace XFSkiaSharpDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SkCanvasView_OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            
+        }
+    }
+}
